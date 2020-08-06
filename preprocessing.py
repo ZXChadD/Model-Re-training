2c53a2069449b1b4115853d17227d274119d8678
ZXChadd <zhexianchadd@gmail.com>
2020-08-06 12:15:55 +0800

    Optimise algo for placing images on background so that they do not overlap

--- preprocessing.py
+++ preprocessing.py
@@ -6,14 +6,87 @@ from PIL import Image
 cifar10_path = 'cifar-10-batches-py'
 width_of_original_image = 32
 height_of_original_image = 32
-max_img_on_bg = 15
-max_images = 21
+max_img_on_bg = 20
+max_images = 101
 
 
 def main():
     create_training_data()
 
 
+def create_training_data():
+    images, labels = load_cfar10_batch(cifar10_path, 1)
+    img_on_bg = 1
+    bg_id = 0
+
+    # list of all images that have been placed on the background
+    all_images = []
+
+    # list of coordinates that should not be chosen from
+    excluded_coordinates = set()
+
+    # create a new background
+    bg = Image.new('RGB', (256, 256), (0, 0, 0))
+
+    # store all possible coordinates
+    all_coordinates = set()
+    for x_coordinates in range(0, 216):
+        for y_coordinates in range(40, 256):
+            coordinates = (x_coordinates, y_coordinates)
+            all_coordinates.add(coordinates)
+
+    for x in range(0, max_images):
+        print("Image Number: " + str(x))
+        label_names = load_label_names()
+        print(label_names[labels[x]])
+        resized_image = resize_image(images[x])
+
+        # once the desired number of images have been placed on the background, create a new background
+        if img_on_bg > max_img_on_bg:
+            bg.save(str(bg_id) + '.png', 'PNG')
+            img_on_bg = 1
+            bg_id += 1
+            bg = Image.new('RGB', (256, 256), (0, 0, 0))
+            all_images = []
+            excluded_coordinates = set()
+
+        img_w, img_h = resized_image.size
+
+        while True:
+
+            # choose coordinates from the remaining set of coordinates
+            remaining_coordinates = all_coordinates - excluded_coordinates
+            offset = random.choice(tuple(remaining_coordinates))
+            x1, y1 = offset
+
+            # position and size of the current image
+            # top left x, top right x, width, height
+            current_image = list(offset)
+            current_image.extend([img_w, img_h])
+
+            if check_for_overlaps(all_images, current_image) and len(excluded_coordinates) != 0 and len(
+                    all_images) != 0:
+                continue
+            else:
+
+                img_on_bg += 1
+
+                # place the image on the background
+                bg.paste(resized_image, (x1, 256 - y1))
+
+                # store the location information of the image
+                all_images.append(current_image)
+
+                # add to the excluded coordinates list
+                for x_coordinates in range(x1, x1 + img_w):
+                    for y_coordinates in range(y1 - img_h, y1):
+                        coordinates = (x_coordinates, y_coordinates)
+                        excluded_coordinates.add(coordinates)
+
+                break
+
+
+# load the cifar dataset
 def load_cfar10_batch(cifar10_path, batch_id):
     with open(cifar10_path + '/data_batch_' + str(batch_id), mode='rb') as file:
         batch = pickle.load(file, encoding='latin1')
@@ -23,61 +96,21 @@ def load_cfar10_batch(cifar10_path, batch_id):
     return images, labels
 
 
-def create_training_data():
-    images, labels = load_cfar10_batch(cifar10_path, 1)
-    img_on_bg = 0
-    bg_id = 0
-    all_images = []
-    bg = Image.new('RGB', (256, 256), (0, 0, 0))
-
-    for x in range(0, max_images):
-        print("Image Number: " + str(x))
-        label_names = load_label_names()
-        print(label_names[labels[x]])
-        resized_image = resize_image(images[x])
-        if img_on_bg < max_img_on_bg:
-            img_on_bg += 1
-        else:
-            bg.save(str(bg_id) + '.png', 'PNG')
-            img_on_bg = 0
-            bg_id += 1
-            bg = Image.new('RGB', (256, 256), (0, 0, 0))
-            all_images = []
-
-        bg_w, bg_h = bg.size
-        img_w, img_h = resized_image.size
-
-        while True:
-
-            x1 = random.randint(0, bg_w - img_w)
-            y1 = random.randint(0 + img_h, bg_h)
-            offset = (x1, y1)
-
-            # position and size of the image
-            # top left x, top right x, width, height
-            current_image = list(offset)
-            current_image.extend([img_w, img_h])
-
-            if check_for_overlaps(all_images, current_image):
-                continue
-            else:
-                # place the image on the canvas
-                bg.paste(resized_image, (x1, 256-y1))
-
-                # store the location information of the image
-                all_images.append(current_image)
-                break
-
-
 # check if new image overlaps with existing images
 def check_for_overlaps(all_images, new_image):
-    # xmin, xmax
+    # x-min, x-max for new image
     new_image_xaxis = (new_image[0], new_image[0] + new_image[2])
-    # ymin, ymax
+
+    # y-min, y-max for new image
     new_image_yaxis = (new_image[1] - new_image[3], new_image[1])
+
     for old_image in all_images:
+
+        # x-min, x-max for old image
         old_image_xaxis = (old_image[0], old_image[0] + old_image[2])
-        old_image_yaxis = (old_image[1] - old_image[3], new_image[1])
+
+        # y-min, y-max for old image
+        old_image_yaxis = (old_image[1] - old_image[3], old_image[1])
 
         if is_overlapping(new_image_xaxis, old_image_xaxis) and is_overlapping(new_image_yaxis, old_image_yaxis):
             return True
@@ -93,9 +126,9 @@ def is_overlapping(image1, image2):
         return False
 
 
-# resize images from a scale of 0.5 to 3
+# resize images from a scale of 0.5 to 1.25
 def resize_image(image):
-    scale = round(random.uniform(0.5, 1.5), 1)
+    scale = round(random.uniform(0.5, 1.25), 1)
     image = Image.fromarray(image)
     new_image = image.resize((round(width_of_original_image * scale), round(height_of_original_image * scale)),
                              Image.BILINEAR)
